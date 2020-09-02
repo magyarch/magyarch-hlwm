c2c4e32ead55b44e878374bb7baf667ff783ee08
bazeeel <deakfefe77@gmail.com>
2020-09-02 08:02:02 +0200

    update

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,7 +8,10 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar --reload mainbar-hlwm &
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload mainbar-hlwm &
+done
+
 
 
 echo "Polybar launched..."
